7da7532b4bb834615de65744e32aa0aed2001f73
Swapnil-197 <sspatil1907@gmail.com>
2024-03-09 21:48:37 +0530

    added getStudentById() along with use of @PathVariable

--- src/main/java/com/sb/controller/StudentController.java
+++ src/main/java/com/sb/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,7 +49,15 @@ public class StudentController {
 	@PostMapping(path = "/addStudent")
 	public Student addStudent(@RequestBody Student student) {
 		System.out.println(student);
+		//hibernate code to store into database
 		return student;
 	}
-
+	
+	//from UI(from POSTMAN we can send data through URL with the help of @PathVariable annotation)
+	@PostMapping(path = "/getStudentById/{studentId}")
+	public Integer getStudentById(@PathVariable Integer studentId) {
+		System.out.println(studentId);
+		//hibernate code to store into database
+		return studentId;
+	}
 }

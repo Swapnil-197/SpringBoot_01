eb280687dfbca37a7b089199b75730e3a798a2e5
Swapnil-197 <sspatil1907@gmail.com>
2024-03-09 22:09:16 +0530

    add getStudentByIdRP() and use of @RequestParam anno

--- src/main/java/com/sb/controller/StudentController.java
+++ src/main/java/com/sb/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sb.pojo.Student;
@@ -60,4 +61,12 @@ public class StudentController {
 		//hibernate code to store into database
 		return studentId;
 	}
+	
+	@PostMapping(path = "/getStudentByIdRP")
+	public Integer getStudentByIdRP(@RequestParam(name="studentId")Integer studentId) {
+		System.out.println(studentId);
+		//hibernate code to store into database
+		return studentId;
+	}
 }
+ 

3ae40849d173b224906d8f9e2aa6d66d1ed3735d
Swapnil-197 <sspatil1907@gmail.com>
2024-03-09 19:33:46 +0530

    added nameOfStudents method and @ReqMap ann on class level

--- src/main/java/com/sb/controller/StudentController.java
+++ src/main/java/com/sb/controller/StudentController.java
@@ -1,14 +1,17 @@
 package com.sb.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sb.pojo.Student;
 
 @RestController
+//@RequestMapping("/student")
 public class StudentController {
 	
 	@GetMapping("/getAllStudents")
@@ -19,7 +22,23 @@ public class StudentController {
 		student.setName("Swapnil");
 		student.setCourse("Java");
 		listOfStudents.add(student);
+		Student student1 = new Student();
+		student1.setSid(2);
+		student1.setName("Nayan");
+		student1.setCourse("Python");
+		listOfStudents.add(student1);
+		Student student3 = new Student();
+		student3.setSid(3);
+		student3.setName("Pavan");
+		student3.setCourse("Ruby");
+		listOfStudents.add(student3);
 		return listOfStudents;
+	}
+	
+	@GetMapping("/nameOfStudents")
+	public List<String> nameOfStudents(){
+		return Arrays.asList("Swapnil","Nayan","Trupti","Shubham");
+		
 	}
 
 }
